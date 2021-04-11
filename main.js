fc5f2e12d58a6f7a2f6fa8da8527449fecfd4c11
shashank725 <shashank7rocks2gmail.com>
2021-04-11 23:57:09 +0530

    country list added\

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const elem = document.getElementById("country-list");
+
 fetch("https://corona-api.com/countries")
   .then((response) => response.json())
   .then((res) =>{
@@ -5,5 +7,10 @@ fetch("https://corona-api.com/countries")
           const li = document.createElement("Button");
           li.id = country.code;
           li.classList.add("country-name");
-          li.innerHTML = `${country code}`
-  })
+          li.innerHTML = `<div class="list-col1">${country.code}</div><div class="list-col2">${country.name}</div><div class="list-col3"><img src="https://www.countryflags.io/${country.code}/flat/32.png"></div>`;
+          li.addEventListener("click", () => {
+              console.log("hi")
+          });
+          elem.appendChild(li);
+      });
+  });
