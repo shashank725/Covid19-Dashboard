931e5a115d9a3ba5f598a6ee6835e922de04d1e5
shashank725 <shashank7rocks2gmail.com>
2021-04-15 00:31:57 +0530

    API added

--- main.js
+++ main.js
@@ -7,10 +7,33 @@ fetch("https://corona-api.com/countries")
           const li = document.createElement("Button");
           li.id = country.code;
           li.classList.add("country-name");
-          li.innerHTML = `<div class="list-col1">${country.code}</div><div class="list-col2">${country.name}</div><div class="list-col3"><img src="https://www.countryflags.io/${country.code}/flat/32.png"></div>`;
+          li.innerHTML = 
+          `<div class="list-col1">${country.code}</div>
+          <div class="list-col2">${country.name}</div>
+          <div class="list-col3"><img src="https://www.countryflags.io/${country.code}/flat/32.png"></div>`;
           li.addEventListener("click", () => {
-              console.log("hi")
+              getData(country.code).then((res) => {
+                  document.getElementById(
+                      "country-selected"
+                      ).innerHTML = `<div>${country.name}</div>`;
+                  displayData(res);
+              })
           });
           elem.appendChild(li);
       });
   });
+
+const getData = async (countryCode) => {
+    const resp = await fetch(`https://corona-api.com/countries/${countryCode}`);
+    const data = await resp.json();
+    return data;
+};
+
+function displayData(resp) {
+    console.log(resp);
+
+    document.getElementById("active.api").innerHTML = `<div>${resp.data.latest_data.critical}</div>`;
+    document.getElementById("confirmed.api").innerHTML = `<div>${resp.data.latest_data.confirmed}</div>`;
+    document.getElementById("recovered.api").innerHTML = `<div>${resp.data.latest_data.recovered}</div>`;
+    document.getElementById("deaths.api").innerHTML = `<div>${resp.data.latest_data.deaths}</div>`;
+}
